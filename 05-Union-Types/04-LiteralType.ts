00a7f546791868cc95ec537bed8060b087f0c2e4
Ujjawal Gupta <ujjawalgupta23@gmail.com>
2023-05-27 01:44:46 +0530

    union type finished

---
+++ 05-Union-Types/04-LiteralType.ts
@@ -0,0 +1,10 @@
+/**
+ * Literal types are not just types but the values themselves too
+ * We can combine it with Union to get the most out of it
+ */
+
+const num1: 0 = 0
+// const num2: 0 = 1 // it must bhi alloted 1
+
+
+
