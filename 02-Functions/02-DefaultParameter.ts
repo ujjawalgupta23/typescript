4abb7160740030b2c6b8dd335283452fda278d32
Ujjawal Gupta <ujjawalgupta23@gmail.com>
2023-05-26 14:50:18 +0530

    default parameter

---
+++ 02-Functions/02-DefaultParameter.ts
@@ -0,0 +1,9 @@
+// We have to provide default parameter after declaring the annotation
+
+function greet (name: string = 'Ujjawal') {
+    console.log(`Hi ${name
+    }`);
+}
+
+greet()
+greet('Ujjawal')
